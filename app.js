43c9d78ec9de7ed0e51eb3543f8aa2b14fbc7705
Shayan56 <shayana.btech22@rvu.edu.in>
2024-11-04 16:08:23 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('USER 32'));
+app.get('/', (req, res) => res.send('Pipeline test done'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
